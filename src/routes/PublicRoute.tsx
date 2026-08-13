cfa96662d83788a6cd99eee4ab3838bfca98b79b
satyam <satyamkumarsingh12a.jssp@gmail.com>
2026-08-13 20:57:38 +0530

    feat:add lazy loading/code spliting

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,19 +1,61 @@
+import { lazy, Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Marketplace } from "../pages/Marketplace";
-import { MarketplaceDetails } from "../pages/MarketplaceDetails";
-import { Community } from "../pages/Community";
-import { CommunityDetails } from "../pages/CommunityDetails";
-import { Collection } from "../pages/Collection";
+import { MarketplaceSkeleton } from "../components/marketplace/MarketplaceSkeleton";
+import { MarketplaceDetailsSkeleton } from "../components/marketplace-details/MarketplaceDetailsSkeleton";
+import { CommunitySkeleton } from "../components/community/CommunitySkeleton";
+import { CommunityDetailsSkeleton } from "../components/community/CommunityDetailsSkeleton";
+
+const MarketplaceDetails = lazy(() =>
+  import("../pages/MarketplaceDetails").then((module) => ({ default: module.MarketplaceDetails }))
+);
+const Community = lazy(() =>
+  import("../pages/Community").then((module) => ({ default: module.Community }))
+);
+const CommunityDetails = lazy(() =>
+  import("../pages/CommunityDetails").then((module) => ({ default: module.CommunityDetails }))
+);
+const Collection = lazy(() =>
+  import("../pages/Collection").then((module) => ({ default: module.Collection }))
+);
 
 export const PublicRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/marketplace" replace />} />
       <Route path="/marketplace" element={<Marketplace />} />
-      <Route path="/marketplace/:id" element={<MarketplaceDetails />} />
-      <Route path="/community" element={<Community />} />
-      <Route path="/community/:id" element={<CommunityDetails />} />
-      <Route path="/collection" element={<Collection />} />
+      <Route
+        path="/marketplace/:id"
+        element={
+          <Suspense fallback={<MarketplaceDetailsSkeleton />}>
+            <MarketplaceDetails />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/community"
+        element={
+          <Suspense fallback={<CommunitySkeleton />}>
+            <Community />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/community/:id"
+        element={
+          <Suspense fallback={<CommunityDetailsSkeleton />}>
+            <CommunityDetails />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/collection"
+        element={
+          <Suspense fallback={<MarketplaceSkeleton />}>
+            <Collection />
+          </Suspense>
+        }
+      />
     </Routes>
   );
 };
